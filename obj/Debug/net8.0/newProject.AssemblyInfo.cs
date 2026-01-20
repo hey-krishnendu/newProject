d66706fd6db78893f18293ed2b57d6becb9daf6e
Krishnendu Smanta <krishnendu665@gmail.com>
2026-01-20 21:54:22 +0530

    Final commit

--- obj/Debug/net8.0/newProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/newProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c1f19565f7817de538c26bbe276a1377aadd368")]
 [assembly: System.Reflection.AssemblyProductAttribute("newProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
